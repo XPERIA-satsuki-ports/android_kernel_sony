5a9ecd15e6775b5b680bd654c9b13c103a87f67a
alucard24 <dmbaoh2@gmail.com>
2016-09-01 21:27:20 +0200

    cpufreq: alucard/darkness: save and restore tuners settings!
    Conflicts:
    	drivers/cpufreq/cpufreq_governor.c

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -37,6 +37,7 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 	struct cs_dbs_tuners *cs_tuners = dbs_data->tuners;
 	struct ex_dbs_tuners *ex_tuners = dbs_data->tuners;
 	struct zz_dbs_tuners *zz_tuners = dbs_data->tuners;
+	struct ac_dbs_tuners *ac_tuners = dbs_data->tuners;
 	struct cpufreq_policy *policy;
 	unsigned int sampling_rate;
 	unsigned int max_load = 0;
@@ -51,10 +52,13 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 	} else if (dbs_data->cdata->governor == GOV_ZZMOOVE) {
 		sampling_rate = zz_tuners->sampling_rate;
 		ignore_nice = zz_tuners->ignore_nice_load;
-	} else {
-		sampling_rate = cs_tuners->sampling_rate;
-		ignore_nice = cs_tuners->ignore_nice_load;
-	}
+	} else if (dbs_data->cdata->governor == GOV_ALUCARD) {
+		sampling_rate = ac_tuners->sampling_rate;
+		ignore_nice = ac_tuners->ignore_nice_load;
+ 	} else {
+ 		sampling_rate = cs_tuners->sampling_rate;
+ 		ignore_nice = cs_tuners->ignore_nice_load;
+ 	}
 	policy = cdbs->cur_policy;
 
 	/* Get Absolute Load */
@@ -191,6 +195,10 @@ static void set_sampling_rate(struct dbs_data *dbs_data,
 	} else if (dbs_data->cdata->governor == GOV_ZZMOOVE) {
 		struct zz_dbs_tuners *zz_tuners = dbs_data->tuners;
 		zz_tuners->sampling_rate = sampling_rate;
+	} else if (dbs_data->cdata->governor == GOV_ALUCARD) {
+		struct ac_dbs_tuners *ac_tuners = dbs_data->tuners;
+		ac_tuners->sampling_rate = max(ac_tuners->sampling_rate, 
+			sampling_rate);
 	} else {
 		struct od_dbs_tuners *od_tuners = dbs_data->tuners;
 		od_tuners->sampling_rate = sampling_rate;
@@ -205,11 +213,14 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 	struct cs_cpu_dbs_info_s *cs_dbs_info = NULL;
 	struct ex_cpu_dbs_info_s *ex_dbs_info = NULL;
 	struct zz_cpu_dbs_info_s *zz_dbs_info = NULL;
+	struct ac_cpu_dbs_info_s *ac_dbs_info = NULL;
 	struct od_ops *od_ops = NULL;
+	struct ac_ops *ac_ops = NULL;
 	struct od_dbs_tuners *od_tuners = NULL;
 	struct cs_dbs_tuners *cs_tuners = NULL;
 	struct ex_dbs_tuners *ex_tuners = NULL;
 	struct zz_dbs_tuners *zz_tuners = NULL;
+	struct ac_dbs_tuners *ac_tuners = NULL;
 	struct cpu_dbs_common_info *cpu_cdbs;
 	unsigned int sampling_rate, latency, ignore_nice, j, cpu = policy->cpu;
 	int io_busy = 0;
@@ -240,7 +251,7 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 		dbs_data->cdata = cdata;
 		dbs_data->usage_count = 1;
-
+				
 		if (cdata->governor == GOV_ELEMENTALX) {
 			rc = cdata->init_ex(dbs_data, policy);
 		} else if (cdata->governor == GOV_ZZMOOVE) {
@@ -349,6 +360,13 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		zz_dbs_info = dbs_data->cdata->get_cpu_dbs_info_s(cpu);
 		sampling_rate = zz_tuners->sampling_rate;
 		ignore_nice = zz_tuners->ignore_nice_load;
+	} else if (dbs_data->cdata->governor == GOV_ALUCARD) {
+		ac_tuners = dbs_data->tuners;
+		ac_dbs_info = dbs_data->cdata->get_cpu_dbs_info_s(cpu);
+		sampling_rate = ac_tuners->sampling_rate;
+		ignore_nice = ac_tuners->ignore_nice_load;
+		ac_ops = dbs_data->cdata->gov_ops;
+	
 	} else {
 		od_tuners = dbs_data->tuners;
 		od_dbs_info = dbs_data->cdata->get_cpu_dbs_info_s(cpu);
@@ -393,6 +411,12 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			zz_dbs_info->down_skip = 0;
 			zz_dbs_info->enable = 1;
 			zz_dbs_info->requested_freq = policy->cur;
+		} else if (dbs_data->cdata->governor == GOV_ALUCARD) {
+			ac_ops->get_cpu_frequency_table(cpu);
+			ac_ops->get_cpu_frequency_table_minmax(policy, cpu);
+			ac_ops->get_cpu_cached_tuners(policy, cpu);
+			ac_dbs_info->up_rate = 1;
+			ac_dbs_info->down_rate = 1;
 		} else {
 			od_dbs_info->rate_mult = 1;
 			od_dbs_info->sample_type = OD_NORMAL_SAMPLE;
@@ -409,8 +433,15 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		break;
 
 	case CPUFREQ_GOV_STOP:
-		if (dbs_data->cdata->governor == GOV_CONSERVATIVE)
+		if (dbs_data->cdata->governor == GOV_CONSERVATIVE) {
 			cs_dbs_info->enable = 0;
+		} else if (dbs_data->cdata->governor == GOV_ALUCARD) {
+			ac_ops->set_cpu_cached_tuners(policy, cpu);
+		} else if (dbs_data->cdata->governor == GOV_DARKNESS) {
+			dk_ops->set_cpu_cached_tuners(policy, cpu);
+		} else if (dbs_data->cdata->governor == GOV_NIGHTMARE) {
+			nm_ops->set_cpu_cached_tuners(policy, cpu);
+		}
 
 		if (dbs_data->cdata->governor == GOV_ELEMENTALX)
 			ex_dbs_info->enable = 0;
